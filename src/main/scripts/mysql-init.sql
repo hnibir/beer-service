5326511fbb0af96da9bcab86b1c9e4a0980b1931
Nibir Hossain <nibir.hossain85@gmail.com>
2020-08-28 15:46:57 +0200

    Add Database init scripts. Closes #23

---
+++ src/main/scripts/mysql-init.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS beerservice;
+
+DROP USER IF EXISTS `user_beer_service`@`%`;
+
+CREATE DATABASE IF NOT EXISTS beerservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE USER IF NOT EXISTS `user_beer_service`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `beerservice`.* TO `user_beer_service`@`%`;
+
+FLUSH PRIVILEGES;
